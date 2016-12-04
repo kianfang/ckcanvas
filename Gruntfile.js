e522dd77f3a2bfb43ce1ed3e53d4fd6355fb0e3e
Kian <kian@vsu.cc>
2016-12-04 16:16:29 +0800

    添加基础目录和必要文件

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            app: {
+                files: {
+                    './dist/jquery.palette.min.js': './src/jquery.palette.js'
+                }
+            }
+        },
+        jshint: {
+            app: ['Gruntfile.js', './src/**/*.js']
+        },
+        copy: {
+            jquery: {
+                files: [{ //jquery1、2、3
+                    expand: true,
+                    flatten: false,
+                    cwd: './bower_components/jquery/dist/',
+                    src: ['jquery.min.js'],
+                    dest: './examples/js/',
+                    filter: 'isFile',
+                }]
+            }
+        },
+        watch: {
+            jquery: {
+                files: ['./bower_components/jquery*/dist/jquery.min.js'], // jquery 监听
+                tasks: ['copy:jquery']
+            }
+        }
+    });
+
+    // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    // 默认被执行的任务列表。
+
+    grunt.registerTask('default', [/*'concat', */'uglify', 'jshint', 'copy']);
+
+};
